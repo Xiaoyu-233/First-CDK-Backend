6cc92a2d04a6391ca8e76281b7695e114fea3566
Xiaoyu-233 <neonyui95@gmail.com>
2021-12-20 01:10:13 +1100

    Writing Lambda function in Typescript by using aws-lambda-nodejs

--- infrastructure/MyStack.ts
+++ infrastructure/MyStack.ts
@@ -4,7 +4,8 @@ import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambd
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
 import { join } from 'path'
 import { GenericTable } from './GenericTable'
-
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { handler } from '../services/node-lambda/hello'
 
 export class MyStack extends Stack{
 
@@ -18,10 +19,17 @@ export class MyStack extends Stack{
     constructor(scope: Construct, id: string, props: StackProps){
         super(scope, id, props)
 
+        // Lambda function written in JS
         const helloLambda = new LambdaFunction(this, 'helloLambda', {
             runtime: Runtime.NODEJS_14_X,
             code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
             handler: 'hello.main'
+        })
+
+        // Lambda function written in TS and compiled by CDK Node Lambda
+        const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
+            entry: (join(__dirname, '..', 'services', 'node-lambda', 'hello.ts')),
+            handler: 'handler'
         })
 
         // Hello API Lambda integration:
