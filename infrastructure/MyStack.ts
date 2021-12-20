e3bd03bc4b8aa4d1ad68029cd3c61beeae9c3042
Xiaoyu-233 <neonyui95@gmail.com>
2021-12-20 17:00:41 +1100

    Using AWS SDK to list s3 buckets inside IDE, and modify the permission

--- infrastructure/MyStack.ts
+++ infrastructure/MyStack.ts
@@ -5,7 +5,7 @@ import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
 import { join } from 'path'
 import { GenericTable } from './GenericTable'
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
-import { handler } from '../services/node-lambda/hello'
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam'
 
 export class MyStack extends Stack{
 
@@ -31,6 +31,11 @@ export class MyStack extends Stack{
             entry: (join(__dirname, '..', 'services', 'node-lambda', 'hello.ts')),
             handler: 'handler'
         })
+        const s3ListPolicy = new PolicyStatement()
+        s3ListPolicy.addActions('s3:ListAllMyBuckets')
+        s3ListPolicy.addResources('*')
+        helloLambdaNodeJs.addToRolePolicy(s3ListPolicy)
+        
 
         // Hello API Lambda integration:
         const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs)
